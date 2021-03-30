a4a8cf8ad3ee86d2a7756c48d1b072fd26ffd5ca
Tiffany Wang <tinnywang@gmail.com>
2021-03-30 02:01:42 -0700

    depth peeling but with z-fighting?

--- src/shaders/image/shader.ts
+++ src/shaders/image/shader.ts
@@ -43,6 +43,7 @@ export class ImageShader extends Shader {
         this.gl.enableVertexAttribArray(this.vertexPosition);
 
         const texture = this.gl.createTexture();
+        this.gl.activeTexture(this.gl.TEXTURE0);
         this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
         this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.drawingBufferWidth, this.gl.drawingBufferHeight, 0, this.gl.RGBA, this.gl.UNSIGNED_BYTE, pixels);
         this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
--- src/shaders/transparent/vertex.glsl
+++ src/shaders/transparent/vertex.glsl
@@ -2,9 +2,12 @@ attribute vec4 vertexPosition;
 
 uniform mat4 modelViewProjectionMatrix;
 
-varying vec2 depthPosition;
+varying highp vec2 texturePosition;
 
 void main(void) {
     gl_Position = modelViewProjectionMatrix * vertexPosition;
-    depthPosition = gl_Position.xy;
+
+    // gl_Position coordinates are in the range [-1, 1].
+    // Texture coordinates should be in the range [0, 1].
+    texturePosition = gl_Position.xy / gl_Position.w * 0.5 + 0.5;
 }
