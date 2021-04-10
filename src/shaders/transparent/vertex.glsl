b8f8335c1c505fe8a10314fcfc094a24059f8ffd
Tiffany Wang <tinnywang@gmail.com>
2021-04-10 16:11:19 -0700

    rewrite shaders as GLSL 3.00

--- src/shaders/transparent/vertex.glsl
+++ src/shaders/transparent/vertex.glsl
@@ -1,13 +1,9 @@
-attribute vec4 vertexPosition;
+#version 300 es
+
+in vec4 vertexPosition;
 
 uniform mat4 modelViewProjectionMatrix;
 
-varying highp vec2 texturePosition;
-
-void main(void) {
+void main() {
     gl_Position = modelViewProjectionMatrix * vertexPosition;
-
-    // gl_Position coordinates are in the range [-1, 1].
-    // Texture coordinates should be in the range [0, 1].
-    texturePosition = gl_Position.xy / gl_Position.w * 0.5 + 0.5;
 }
