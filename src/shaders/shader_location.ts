9d5eaeaba586e0f4faad024e542b131d4e960933
Tiffany Wang <tinnywang@gmail.com>
2021-06-27 22:59:11 -0700

    add new class to encapsulate shader locations

---
+++ src/shaders/shader_location.ts
@@ -0,0 +1,32 @@
+export class ShaderLocation {
+	private gl: WebGL2RenderingContext;
+	private program: WebGLProgram;
+
+	readonly uniforms: Map<string, WebGLUniformLocation | null>;
+	readonly attributes: Map<string, GLint>;
+
+	constructor(gl: WebGL2RenderingContext, program: WebGLProgram) {
+		this.gl = gl;
+		this.program = program;
+		this.uniforms = new Map<string, WebGLUniformLocation | null>();
+		this.attributes = new Map<string, GLint>();
+	}
+
+	getUniform(name: string): WebGLUniformLocation | null {
+		return this.uniforms.get(name) ?? null;
+	}
+
+	setUniform(name: string) {
+		const location = this.gl.getUniformLocation(this.program, name);
+		this.uniforms.set(name, location);
+	}
+
+	getAttribute(name: string): GLint {
+		return this.attributes.get(name) ?? -1;
+	}
+
+	setAttribute(name: string) {
+		const location = this.gl.getAttribLocation(this.program, name);
+		this.attributes.set(name, location);
+	}
+}
