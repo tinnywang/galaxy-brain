56955ce0c731923edeed0dca063e23fa71960d5d
Tiffany Wang <tinnywang@gmail.com>
2022-12-13 23:04:22 -0800

    create textures from images

--- src/gl.ts
+++ src/gl.ts
@@ -44,6 +44,34 @@ class WebGL2 {
     );
   }
 
+  static createImageTexture(
+    gl: WebGL2RenderingContext,
+    src: string
+  ): WebGLTexture {
+    const texture = gl.createTexture();
+    if (texture === null) {
+      throw new Error("Unable to create texture.");
+    }
+
+    const image = new Image();
+    image.src = src;
+    image.onload = () => {
+      gl.bindTexture(gl.TEXTURE_2D, texture);
+      gl.texImage2D(
+        gl.TEXTURE_2D,
+        0,
+        gl.RGBA,
+        gl.RGBA,
+        gl.UNSIGNED_BYTE,
+        image
+      );
+      // The image's dimensions must be a power of 2.
+      gl.generateMipmap(gl.TEXTURE_2D);
+    };
+
+    return texture;
+  }
+
   static createTextures(
     gl: WebGL2RenderingContext,
     n: number,
---
+++ src/types.d.ts
@@ -0,0 +1,9 @@
+declare module "*.glsl" {
+  const shader: string;
+  export default shader;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
