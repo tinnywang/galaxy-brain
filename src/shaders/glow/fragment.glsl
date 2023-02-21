dc4e5b71d1c2807943d90ae84121d2197c12262f
Tiffany Wang <tinnywang@gmail.com>
2023-02-20 17:09:21 -0800

    mix colors in glow shader

--- src/shaders/glow/fragment.glsl
+++ src/shaders/glow/fragment.glsl
@@ -7,15 +7,16 @@ in float fragDepth;
 out vec4 fragColor;
 
 uniform vec3 color;
-uniform float radius;
 
 void main(void) {
     gl_FragDepth = fragDepth;
 
     float dist = distance(gl_PointCoord, vec2(0.5, 0.5));
-    float falloff = 1.0 - clamp(dist, 0.0, 1.0);
-    vec4 innerColor = vec4(1.0) * pow(falloff, dist * radius);
-    vec4 outerColor = vec4(color, 1.0) * falloff;
-    // fragColor = vec4(innerColor + outerColor, falloff);
-    fragColor = innerColor;
+    float falloff = 1.0 - smoothstep(0.0, 0.5, dist);
+    float alpha = pow(falloff, dist);
+
+    vec4 innerColor = vec4(1, 1, 1, alpha);
+    vec4 outerColor = vec4(color, alpha);
+
+    fragColor = mix(outerColor, innerColor, falloff);
 }
