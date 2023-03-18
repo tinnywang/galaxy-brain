aa43fc7aa64eb4c80d837cebc4b9a7c58b2cb6c9
Tiffany Wang <tinnywang@gmail.com>
2023-03-18 02:33:38 -0700

    use lens flare texture for glow

--- src/shaders/glow/shader.ts
+++ src/shaders/glow/shader.ts
@@ -2,11 +2,16 @@ import fragmentSrc from './fragment.glsl';
 import vertexSrc from './vertex.glsl';
 import { Light } from '../../light';
 import { Shader } from '../shader';
+import WebGL2 from '../../gl';
+import Image from "../../assets/glow.png";
 
 export class Glow extends Shader<Light> {
+    private lightTexture: WebGLTexture;
 
     constructor(gl: WebGL2RenderingContext) {
         super(gl, vertexSrc, fragmentSrc);
+
+        this.lightTexture = WebGL2.createImageTexture(gl, Image);
 
         this.locations.setAttribute('vertexPosition');
         this.locations.setUniform('modelViewMatrix');
@@ -14,12 +19,21 @@ export class Glow extends Shader<Light> {
 
         this.locations.setUniform('radius');
         this.locations.setUniform('color');
+
+        this.locations.setUniform('lightTexture');
     }
 
     render(drawFramebuffer: WebGLFramebuffer, ...lights: Light[]) {
+        this.gl.bindFramebuffer(this.gl.DRAW_FRAMEBUFFER, drawFramebuffer);
+
         super.render(drawFramebuffer, ...lights);
 
-        this.gl.bindFramebuffer(this.gl.DRAW_FRAMEBUFFER, drawFramebuffer);
+        this.gl.enable(this.gl.BLEND);
+        this.gl.blendFunc(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA);
+
+        this.gl.activeTexture(this.gl.TEXTURE0);
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.lightTexture);
+        this.gl.uniform1i(this.locations.getUniform('lightTexture'), 0);
 
         lights?.forEach((l) => l.render(this.gl, this.locations));
     }
--- src/shaders/glow/vertex.glsl
+++ src/shaders/glow/vertex.glsl
@@ -2,16 +2,12 @@
 
 in vec4 vertexPosition;
 
-out highp float fragDepth;
-
 uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
 uniform float radius;
 
 void main(void) {
     gl_Position = projectionMatrix * modelViewMatrix * vertexPosition;
-
-    fragDepth = gl_Position.z / gl_Position.w;
 
     gl_PointSize = radius;
 }
