970cb64d27c443c302be67fb4686112dd1739cc7
Tiffany Wang <tinnywang@gmail.com>
2023-10-08 17:07:18 -0700

    revisit webpack

---
+++ src/matrix-rain.d.ts
@@ -0,0 +1,10 @@
+declare module "@ojdom/matrix-rain" {
+  export class MatrixController {
+    constructor(options?: {}, canvas?: HTMLCanvasElement);
+    start: () => void;
+
+    disable: () => void;
+
+    stop: () => void;
+  }
+}
